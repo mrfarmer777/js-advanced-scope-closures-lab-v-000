5e0b015baf28cda4a4cfa3754f19f23ce39613e5
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 18:23:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ function produceDrivingRange(range){
   return function(str,end){
     let dist=Math.abs(parseInt(end)-parseInt(str));
     if(range>=dist){
-      return `within range by ${dist}`;
+      return `within range by ${range-dist}`;
     } else {
-      return `out of range by ${dist}`;
+      return `out of range by ${dist-range}`;
     }
   };
 };
