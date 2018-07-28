ecbecb9724f32dab5fd280ed7666d81a50dd335e
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 18:23:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 function produceDrivingRange(range){
   return function(str,end){
-    let dist=parseInt(end)-parseInt(str);
+    let dist=Math.abs(parseInt(end)-parseInt(str))
     if(range>=dist){
       return `within range by ${dist}`;
     } else {
       return `out of range by ${dist}`;
     }
   };
-}
+};
