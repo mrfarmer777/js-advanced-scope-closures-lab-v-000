e965d245f03be1b4f801ac6f9ceea239dc27350b
Matt Farmer <mrfarmer777@gmail.com>
2018-07-28 18:28:59 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function produceDrivingRange(range){
+  return function(str,end){
+    let dist=Math.abs(parseInt(end)-parseInt(str));
+    if(range>=dist){
+      return `within range by ${range-dist}`;
+    } else {
+      return `${dist-range} blocks out of range`;
+    }
+  };
+}
+
+function produceTipCalculator(perc){
+  return function(fare){
+    return fare*perc;
+  };
+}
+
+function createDriver(){
+  let driverId=0;
+  return class Driver{
+    constructor(name){
+      this.id=++driverId;
+      this.name=name;
+    }
+  }
+}
